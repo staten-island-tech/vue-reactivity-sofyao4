6c733eaea1aacca06ebebf23ee8e36f6c80e6489
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-03-10 13:13:22 -0500

    3/10

---
+++ vue-reactivity/src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+};
+</script>
+
+<style scoped>
+button {
+  color: pink;
+  font-size: 15px;
+}
+</style>
---
+++ vue-reactivity/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home-view"></div>
+  <Button />
+</template>
+
+<script>
+import Button from "../components/Button.vue";
+export default {
+  name: "Home",
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
